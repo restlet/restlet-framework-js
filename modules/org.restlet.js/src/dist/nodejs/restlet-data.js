155d89eb31805df66fe654fdcd3d2a63f75fac39
Thierry Templier <templth@yahoo.fr>
2012-04-04 13:53:50 +0200

    Moved browser template file Added nodejs template files

---
+++ modules/org.restlet.js/src/dist/nodejs/restlet-data.js
@@ -0,0 +1,68 @@
+#include org/restlet/data/Protocol.js#
+
+#include org/restlet/data/ClientInfo.js#
+
+#include org/restlet/data/ServerInfo.js#
+
+#include org/restlet/data/Reference.js#
+
+#include org/restlet/data/Method.js#
+
+#include org/restlet/data/CacheDirective.js#
+
+#include org/restlet/data/CharacterSet.js#
+
+#include org/restlet/data/Cookie.js#
+
+#include org/restlet/data/CookieSetting.js#
+
+#include org/restlet/data/Conditions.js#
+
+#include org/restlet/data/Dimension.js#
+
+#include org/restlet/data/Disposition.js#
+
+#include org/restlet/data/Parameter.js#
+
+#include org/restlet/data/Metadata.js#
+
+#include org/restlet/data/Encoding.js#
+
+#include org/restlet/data/Form.js#
+
+#include org/restlet/data/Language.js#
+
+#include org/restlet/data/Preference.js#
+
+#include org/restlet/data/Range.js#
+
+#include org/restlet/data/RecipientInfo.js#
+
+#include org/restlet/data/Tag.js#
+
+#include org/restlet/data/Warning.js#
+
+exports = {
+	Protocol: Protocol,
+	ClientInfo: ClientInfo,
+	ServerInfo: ServerInfo,
+	Reference: Reference,
+	Method: Method,
+	CacheDirective: CacheDirective,
+	CharacterSet: CharacterSet,
+	Cookie: Cookie,
+	CookieSetting: CookieSetting,
+	Conditions: Conditions,
+	Dimension: Dimension,
+	Disposition: Disposition,
+	Parameter: Parameter,
+	Metadata: Metadata,
+	Encoding: Encoding,
+	Form: Form,
+	Language: Language,
+	Preference: Preference,
+	Range: Range,
+	RecipientInfo: RecipientInfo,
+	Tag: Tag,
+	Warning: Warning
+}
