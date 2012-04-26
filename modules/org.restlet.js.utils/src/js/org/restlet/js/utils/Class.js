e23638ea73ee069748fb1e9f46f699be18e78b63
Thierry Templier <templth@yahoo.fr>
2012-04-26 20:41:15 +0200

    Handled super constructor and super calls

--- modules/org.restlet.js.utils/src/js/org/restlet/js/utils/Class.js
+++ modules/org.restlet.js.utils/src/js/org/restlet/js/utils/Class.js
@@ -1,12 +1,9 @@
-function copyElements(obj1, obj2) {
+function copyElements(obj1, obj2, parent) {
 	for (var elt in obj1) {
-		//console.log("copy "+elt+" ("+typeof obj1[elt]+")");
-		if (elt!="initialize" || typeof obj1[elt] != "function") {
-			//console.log("  -> ok");
-			obj2[elt] = obj1[elt];
-		} else {
-			obj2["_"+elt] = obj1[elt];
+		if (parent!=null && obj2[elt]!=null && typeof obj2[elt] == "function") {
+			parent[elt] = obj2[elt];
 		}
+		obj2[elt] = obj1[elt];
 	}
 }
 
@@ -21,7 +18,6 @@ var Class = function() {
 	}
 	
 	var clazz = function() {
-		//console.log("clazz.initializeExtend = "+clazz.initializeExtend);
 		if (clazz.initializeExtend!=null && clazz.initializeExtend==true) {
 			return;
 		}
@@ -30,41 +26,53 @@ var Class = function() {
 		}
 	}
 	if (parent!=null) {
-		copyElements(parent, clazz);
 		parent.initializeExtend = true;
-		//console.log("> new parent");
 		clazz.prototype = new parent();
-		//console.log("< new parent");
-		clazz.parent = parent.prototype;
+		clazz.prototype.__parent = parent.prototype;
+		parent.prototype.__child = clazz.prototype;
 		parent.initializeExtend = null;
-		copyElements(content, clazz.prototype);
-		//console.log("adding call super");
-		clazz.prototype["callSuper"] = function() {
-			//console.log("clazz.parent.prototype = "+clazz.parent.prototype);
-			/*console.log("clazz.parent = "+clazz.parent);
-			for(var elt in clazz.parent) {
-				console.log("- elt "+elt);
-			}*/
-			if (clazz.parent["_initialize"]!=null) {
-				var superInitialize = clazz.parent["_initialize"];
-				/*console.log("superInitialize = "+superInitialize);
-				console.log("this = "+this);*/
+		copyElements(content, clazz.prototype, clazz.prototype.__parent);
+		clazz.prototype["callSuperCstr"] = function() {
+			if (this.__currentCallSuperLevel==null) {
+				this.__currentCallSuperLevel = this.__parent;
+			} else {
+				this.__currentCallSuperLevel = this.__currentCallSuperLevel.__parent;
+			}
+			
+			var currentLevel = this.__currentCallSuperLevel;
+			if (currentLevel["initialize"]!=null && typeof currentLevel["initialize"]=="function") {
+				var superInitialize = currentLevel["initialize"];
 				superInitialize.apply(this, arguments);
 			}
+			this.__currentCallSuperLevel = this.__currentCallSuperLevel.__child;
 		};
-		//console.log("added call super");
+		clazz.prototype["callSuper"] = function() {
+			if (this.__currentCallSuperLevel==null) {
+				this.__currentCallSuperLevel = this.__parent;
+			} else {
+				this.__currentCallSuperLevel = this.__currentCallSuperLevel.__parent;
+			}
+			
+			var currentLevel = this.__currentCallSuperLevel;
+			var methodName = arguments[0];
+			if (currentLevel[methodName]!=null && typeof currentLevel[methodName]=="function") {
+				var args = [];
+				for (var i=1;i<arguments.length;i++) {
+					args.push(arguments[i]);
+				}
+				currentLevel[methodName].apply(this, args);
+			}
+			this.__currentCallSuperLevel = this.__currentCallSuperLevel.__child;
+		};
 	} else {
 		clazz.prototype = {};
 		copyElements(content, clazz.prototype);
 	}
-	//copyElements(parent, clazz);
-	/*console.log("clazz.prototype:");
-	for (var elt in clazz.prototype) {
-		console.log(" - "+elt);
-	}*/
 
 	clazz.extend = function(content) {
 		copyElements(content, this);
 	};
 	return clazz;
 };
+
+module.exports = Class;
