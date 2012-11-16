e31446c2f886a3a00c37d6641617dd5d21631a49
Thierry Templier <templth@yahoo.fr>
2012-11-16 16:41:01 +0100

    Updated tests for Node

--- tests/org.restlet.js.tests/src/nodejs/reference-tests.js
+++
@@ -1,9 +0,0 @@
-var restlet = require("restlet");
-var assert = require("assert");
-
-var ref = new restlet.data.Reference("http://localhost:8182/contact/1");
-console.log(ref.getScheme());
-assert.ok(ref.getScheme(), "http");
-assert.ok(ref.getHost(), "localhost");
-assert.ok(ref.getPort(), "8182");
-assert.ok(ref.getPath(), "/contact/1");
