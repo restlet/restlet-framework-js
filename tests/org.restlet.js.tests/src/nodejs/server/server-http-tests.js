a4f525d9e54e36799e274fd074783d7d1f68f382
Thierry Templier <templth@yahoo.fr>
2012-11-21 09:49:53 +0100

    Updated tests for server side

--- tests/org.restlet.js.tests/src/nodejs/server/server-http-tests.js
+++ tests/org.restlet.js.tests/src/nodejs/server/server-http-tests.js
@@ -14,12 +14,23 @@ var application = new restlet.Application();
 application.createInboundRoot = function() {
 	var router = new restlet.Router();
 	router.appRouter = true;
-	var r = new restlet.Filter();
-	r.handle = function(request, response) {
-		console.log("r.handle");
-		response.end();
+	//r1
+	var r1 = new restlet.Filter();
+	r1.handle = function(request, response) {
+		console.log("---> r1.handle");
+		var repr = new restlet.representation.StringRepresentation("test");
+		repr.setAvailable(true);
+		response.setEntity(repr);
+		//response.end();
 	};
-	router.attach("/test", r);
+	router.attach("/test1", r1);
+	//r2
+	var r2 = new restlet.Filter();
+	r2.handle = function(request, response) {
+		console.log("---> r2.handle");
+		//response.end();
+	};
+	router.attach("/test2", r2);
 	return router;
 };
 component.getDefaultHost().attachDefault(application);
