6b5eb542952c91d0fce70440c8208a0286835421
Thierry Templier <templth@yahoo.fr>
2015-06-03 19:20:33 +0200

    feat(samples): updated client sample

--- samples/helloworld/client.js
+++ samples/helloworld/client.js
@@ -1,6 +1,33 @@
 var restlet = require('../../index');
 
 restlet.createClientResource('http://localhost:3000/contacts/')
-.get(function(request, response) {
-  console.log('>> response.entity.text = ' + response.entity.text);
+.get({
+  accept: 'application/yaml',
+  parameters: [ 'entity' ],
+  convertInputEntity: true,
+  queryParameters: {
+  	test1: 'test value1',
+  	test2: 'test value2'
+  }
+},
+	function(entity) {
+  console.log('>> entity = ' + JSON.stringify(entity));
+})
+.post({
+  id: '4',
+  lastName: 'ln1',
+  firstName: 'fn1',
+  age: 111
+}, {
+  accept: 'application/yaml',
+  mediaType: 'application/json',
+  parameters: [ 'response' ],
+  convertOutputEntity: true,
+  queryParameters: {
+  	test1: 'test value1',
+  	test2: 'test value2'
+  }
+},
+	function(response) {
+  console.log('>> received response');
 });
