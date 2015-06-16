73f2088c09cbce934db0fbf03daddfc0564ffe51
Thierry Templier <templth@yahoo.fr>
2015-06-16 15:04:49 +0200

    feature(converter): added built-in converters for both node and browser

---
+++ browser/builtin-converters.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var _ = require('lodash');
+
+function checkMediaTypes(mediaTypes, supportedMediaTypes) {
+  var match = false;
+  _.forEach(mediaTypes, function(mediaType) {
+    _.forEach(supportedMediaTypes, function(supportedMediaType) {
+      if (mediaType.name == supportedMediaType) {
+        match = true;
+      }
+    });
+  });
+  return match;
+}
+
+var converters = [];
+
+converters.push({
+  name: 'json',
+  apply: function(mediaTypes) {
+    return checkMediaTypes(mediaTypes, [ 'application/json' ]);
+  },
+
+  toObject: function(string, callback) {
+    if (!_.isEmpty(string)) {
+      try {
+        callback(null, JSON.parse(string));
+      } catch(err) {
+        callback(err, null);
+      }
+    } else {
+      callback(null, null);
+    }
+  },
+
+  toString: function(obj, callback) {
+    callback(null, JSON.stringify(obj), { name: 'application/json' });
+  }
+});
+
+module.exports = converters;
